1b55fb0082ac68c898cfce4f7202f5078acfe0cb
mpilarmartinez <mariapilarmartinezjarque@gmail.com>
2022-11-15 00:06:32 +0100

    Interface ITaskRepository

---
+++ backend/Repositories/ITaskRepository.cs
@@ -0,0 +1,32 @@
+﻿using Task = backend.Models.Task;
+
+namespace backend.Repositories;
+
+    public interface ITaskRepository
+    {
+        // buscar por id
+        Task FindById(int id);
+
+        // buscar  todos
+        List<Task> FindAll();
+
+        // buscar por  importancia
+        List<Task> FindByImportanceTask(string importancetask);
+
+        // buscar por status
+        List<Task> FindByStatus(string statustask);
+
+        // buscar por id incluyendo asociaciones: Project, TaskDepartment
+        Task FindByIdWithInclude(int id);
+
+        //Guardar nuevo
+        Task CreateTask(Task task);
+
+        //Actualizar Task
+        Task UpdateTask(Task task);
+
+        // borrar por id
+        bool Remove(int id);
+
+    }
+
