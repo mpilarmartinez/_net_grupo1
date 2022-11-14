cc171c18b36672e2a7a6b90fecd2bda0d4ecb756
yessid-22 <equipo221@hotmail.com>
2022-11-14 22:56:33 +0100

    Crear clase TaskDepartmnt

---
+++ backend/Models/TaskDepartment.cs
@@ -0,0 +1,24 @@
+﻿namespace backend.Models;
+
+//nombre de la tabla
+[Table("TaskDepartment")]
+
+public class TaskDepartment
+{
+    // atributos
+    [Key, Column("ID_DEPARTMENT", Order = 0)]
+    public int IdDepartment { get; set; }
+
+    [Column("NAME_DEPARTMENT", TypeName = "varchar(75)", Order = 2)]
+    public string NameDepartment { get; set; }
+
+    //Asociasion Many to One un autor asocia a mas de un libro "TablaId" = Autor columna id
+    //public Autor Autor { get; set; }
+    //public int AutorId { get; set; }
+
+    // ToString
+    /*public override string ToString()
+    {
+        return $"TaskDepartment: {Id}, NameDepartment:{NameDepartment}";
+    }*/
+}
