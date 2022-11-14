141161a43760e3c9c805928b1162929384c558a6
yessid-22 <equipo221@hotmail.com>
2022-11-14 22:59:13 +0100

    Crear interface ITaskDepartmentRepository

---
+++ backend/Repositories/ITaskDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace backend.Repositories;
+
+public interface ITaskDepartmentRepository
+{
+    //Metodos
+
+    //Buscar por id
+    TaskDepartment FindByIdDepartment(int id);
+    List<TaskDepartment> FindAllDepartment();
+
+    //Guardar nuevo
+    TaskDepartment CreateDepartment(TaskDepartment TaskDepartment);
+
+    //Actualizar TaskDepartmen existente
+    TaskDepartment UpdateDepartment(TaskDepartment taskDepartment);
+
+    //Eliminar por id
+    bool RemoveByIdDepartment(int id);
+}
