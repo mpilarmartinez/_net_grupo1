720c300ff495c732f273395e4d222556aa6bcb17
yessid-22 <equipo221@hotmail.com>
2022-11-15 03:19:26 +0100

    Crear clase TaskDepartmentController

---
+++ backend/Controllers/TaskDepartmentController.cs
@@ -0,0 +1,50 @@
+﻿using backend.Repositories;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/taskDepartments")]
+public class TaskDepartmentController
+{
+    // atributos
+    private ITaskDepartmentRepository TaskDepartmentRepo;
+
+    // constructores
+    public TaskDepartmentController(ITaskDepartmentRepository taskDepartmentRepository)
+    {
+        TaskDepartmentRepo = taskDepartmentRepository;
+    }
+
+    // métodos
+
+    [HttpGet] // GET api/taskDepartments
+    public List<TaskDepartment> FindAllDepartment()
+    {
+        return TaskDepartmentRepo.FindAllDepartment();
+    }
+
+    [HttpGet("{id}")] // GET api/taskDepartments/3
+    public TaskDepartment FindByIdDepartment(int id)
+    {
+        return TaskDepartmentRepo.FindByIdDepartment(id);
+    }
+
+    [HttpPost] // POST api/taskDepartments
+    public TaskDepartment CreateDepartment(TaskDepartment taskDepartment)
+    {
+        var TaskDepartmentDB = TaskDepartmentRepo.CreateDepartment(taskDepartment);
+        return TaskDepartmentDB;
+    }
+
+    [HttpPut] // POST api/taskDepartments
+    public TaskDepartment UpdateDepartment(TaskDepartment taskDepartment)
+    {
+        return TaskDepartmentRepo.UpdateDepartment(taskDepartment);
+    }
+
+    [HttpDelete("{id}")]
+    public void DeleteById(int id)
+    {
+        TaskDepartmentRepo.RemoveByIdDepartment(id);
+    }
+}
