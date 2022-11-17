72b0e2ba3449257ced56ad2bccf94cb74321e711
IvanSerran <ivseru11@hotmail.com>
2022-11-17 18:16:50 +0100

    Implementación métodos User

--- backend/Repositories/IUserRepository.cs
+++ backend/Repositories/IUserRepository.cs
@@ -2,5 +2,19 @@
 {
     public interface IUserRepository
     {
+        //Buscar Usuario por ID
+        User FindByID(int ID);
+
+        //Mostrar todos los usuarios
+        List<User> FindAll();
+
+        //Crear un usuario
+        User Create(User user);
+
+        //Modificar un usuario existente
+        User Update(User user);
+
+        //Eliminar un usuario por ID
+        bool Delete(int id);
     }
 }
