f1e61fd2e25195267a66fd2288fd169a5d4018ba
yessid-22 <equipo221@hotmail.com>
2022-11-19 21:26:42 +0100

    Modulos a usar boton, table y icono

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//modulo boton de la libreria material
+import { MatButtonModule } from '@angular/material/button';
+//modulo tabla de la libreria material
+import { MatTableModule } from '@angular/material/table';
+//modulo icon de la libreria material
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    //añadido
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
