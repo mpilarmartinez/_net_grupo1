3cace93d7ff776c3ebbed6097e2abbf3b91016af
mpilarmartinez <mariapilarmartinezjarque@gmail.com>
2022-11-24 23:41:04 +0100

    Generado Project List

--- frontend/src/app/project-list/project-list.component.ts
+++ frontend/src/app/project-list/project-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/project.model';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-project-list',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectListComponent implements OnInit {
 
-  constructor() { }
+  projects: Project[] = [];
+  // debe coincidir con json
+  columnNames: string[] = ['IdProject', 'NameProject', 'StatusProject'];
+
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    //añade método a usar
+    this.findAll();
+  }
+
+  private findAll() {
+    //subscribe para ver los resultados
+    this.projectService.findAllProject().subscribe(
+      {
+        //si va bien se ejecuta
+        next: projects => this.projects = projects,
+        //si va mal se ejecuta
+        error: err => console.log(err)
+      }
+    );
   }
 
 }
+
