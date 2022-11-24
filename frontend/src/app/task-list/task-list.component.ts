ee05c43af32ea9d0d9817431b27e7d021acc9385
mpilarmartinez <mariapilarmartinezjarque@gmail.com>
2022-11-24 23:23:18 +0100

    Generado Task List

--- frontend/src/app/task-list/task-list.component.ts
+++ frontend/src/app/task-list/task-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task.model';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-task-list',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskListComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[] = [];
+  // debe coincidir con json
+  columnNames: string[] = ['IdTask', 'NameTask', 'DateTask', 'StatusTask', 'ImportanceTask'];
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    //añade método a usar
+    this.findAll();
+  }
+
+  private findAll() {
+    //subscribe para ver los resultados
+    this.taskService.findAllTask().subscribe(
+      {
+        //si va bien se ejecuta
+        next: tasks => this.tasks = tasks,
+        //si va mal se ejecuta
+        error: err => console.log(err)
+      }
+    );
   }
 
 }
+
