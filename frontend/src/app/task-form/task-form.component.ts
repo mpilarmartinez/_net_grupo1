d71ca5d557eaca47fae42f826b40c9ea245de78d
mpilarmartinez <mariapilarmartinezjarque@gmail.com>
2022-11-25 23:02:09 +0100

    Generado Task Form

--- frontend/src/app/task-form/task-form.component.ts
+++ frontend/src/app/task-form/task-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-task-form',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskFormComponent implements OnInit {
 
-  constructor() { }
+  editForm = this.createFormGroup();
+  error: boolean = false;
+
+  constructor(private taskService: TaskService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
   }
 
+
+  createFormGroup() {
+    return new FormGroup({
+      IdTask: new FormControl({ value: null, disabled: true }),
+      NameTask: new FormControl('', { nonNullable: true }),
+      DateTask: new FormControl('', { nonNullable: true }),
+      StatusTask: new FormControl('', { nonNullable: true }),
+      ImportanceTask: new FormControl('', { nonNullable: true }),
+    });
+  }
+
+  save() {
+    let task = {
+      NameTask: this.editForm.get("NameTask")?.value,
+      DateTask: this.editForm.get("DateTask")?.value,
+      StatusTask: this.editForm.get("StatusTask")?.value,
+      ImportanceTask: this.editForm.get("ImportanceTask")?.value,
+    } as any;
+
+    let id = this.editForm.get("id")?.value;
+    if (id) {
+      task.IdTask = id;
+
+      this.taskService.updateTask(task).subscribe({
+        next: response => this.navigateToList(),
+        error: err => this.showError(err)
+      });
+    }
+  }
+
+  private showError(err: any): void {
+    console.log(err);
+    this.error = true;
+  }
+
+  private navigateToList() {
+    this.router.navigate(["/sers"]);
+  }
 }
