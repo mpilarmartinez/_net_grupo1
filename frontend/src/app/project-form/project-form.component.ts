dbe9f4cfec262fd9ae55e02fc44e132144e005f7
mpilarmartinez <mariapilarmartinezjarque@gmail.com>
2022-11-25 23:24:38 +0100

    Generado Proyect Form

--- frontend/src/app/project-form/project-form.component.ts
+++ frontend/src/app/project-form/project-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-project-form',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectFormComponent implements OnInit {
 
-  constructor() { }
+  editForm = this.createFormGroup();
+  error: boolean = false;
+
+  constructor(private projectService: ProjectService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
   }
 
+  createFormGroup() {
+    return new FormGroup({
+      IdProject: new FormControl({ value: null, disabled: true }),
+      NameProject: new FormControl('', { nonNullable: true }),
+      StatusProject: new FormControl('', { nonNullable: true }),
+    });
+  }
+
+  save() {
+    let project = {
+      NameProject: this.editForm.get("NameProject")?.value,
+      StatusProject: this.editForm.get("StatusProject")?.value,
+    } as any;
+
+    let id = this.editForm.get("IdProject")?.value;
+    if (id) {
+      project.IdProject = id;
+
+      this.projectService.updateProject(project).subscribe({
+        next: response => this.navigateToList(),
+        error: err => this.showError(err)
+      });
+    }
+  }
+
+  private showError(err: any): void {
+    console.log(err);
+    this.error = true;
+  }
+
+  private navigateToList() {
+    this.router.navigate(["/sers"]);
+  }
 }
+
