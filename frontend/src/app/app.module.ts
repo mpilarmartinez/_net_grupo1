d3953af5714f0d586bcb9dbcd81a93f7c7f40896
IvanSerran <ivseru11@hotmail.com>
2022-11-25 18:08:19 +0100

    Componente UserDetail creado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,31 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//modulo boton de la libreria material
+//modulos de angular-material
 import { MatButtonModule } from '@angular/material/button';
-//modulo tabla de la libreria material
 import { MatTableModule } from '@angular/material/table';
-//modulo icon de la libreria material
 import { MatIconModule } from '@angular/material/icon';
-//modulo select
 import { MatSelectModule } from '@angular/material/select';
-//modulo card
 import { MatCardModule } from '@angular/material/card';
-//modulo para detalles
 import { MatDividerModule } from '@angular/material/divider';
-//modulo para formulario
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
 
 //modulo router
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-//auto creado
+//Componentes creados
 import { TaskdepartmentListComponent } from './taskdepartment-list/taskdepartment-list.component';
 import { TaskdepartmentDetailComponent } from './taskdepartment-detail/taskdepartment-detail.component';
 import { TaskdepartmentFormComponent } from './taskdepartment-form/taskdepartment-form.component';
@@ -34,10 +25,10 @@ import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { ProjectFormComponent } from './project-form/project-form.component';
-//import { ProjectEditComponent } from './project-edit/project-edit.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
@@ -50,20 +41,19 @@ import { UserListComponent } from './user-list/user-list.component';
     TaskDetailComponent,
     TaskFormComponent,
     ProjectFormComponent,
-    //ProjectEditComponent,
     ProjectDetailComponent,
     UserFormComponent,
-    UserListComponent
+    UserListComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
 
-    //añadido
     MatButtonModule,
     MatTableModule,
     MatIconModule,
-    HttpClientModule,//aqui con angular llega a los controladores
+    HttpClientModule,
     MatSelectModule,
     MatCardModule,
     MatDividerModule,
