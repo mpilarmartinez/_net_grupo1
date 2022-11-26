c08601dc3e1775728675e97f35d2b9131c38ffd6
mpilarmartinez <mariapilarmartinezjarque@gmail.com>
2022-11-27 00:16:21 +0100

    Generado Project Detail

--- frontend/src/app/project-detail/project-detail.component.ts
+++ frontend/src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Project } from '../models/project.model';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-project-detail',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailComponent implements OnInit {
 
-  constructor() { }
+  project: Project | undefined;
+
+  constructor(private projectService: ProjectService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe({
+      next: pmap => {
+        let id = pmap.get("id");
+        this.fetchUser(id);
+      },
+      error: err => console.log(err)
+    });
+  }
+  fetchUser(id: string | null) {
+    this.projectService.findByIdProject(Number(id)).subscribe({
+      next: projectBackend => this.project = projectBackend,
+      error: err => console.log(err)
+    });
   }
 
+  onDelete(id: number) {
+    console.log(id);
+    this.projectService.deleteByIdProject(id).subscribe({
+      next: response => this.navigateToList(),
+      error: err => console.log(err)
+    });
+  }
+
+  private navigateToList() {
+    this.router.navigate(["/projects"]);
+  }
 }
