6446698555ba315944c8c79a9cfa80911070de7b
yessid-22 <equipo221@hotmail.com>
2022-11-27 01:13:23 +0100

    pagina detalle departamento

--- frontend/src/app/taskdepartment-detail/taskdepartment-detail.component.ts
+++ frontend/src/app/taskdepartment-detail/taskdepartment-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Taskdepartment } from '../models/taskdepartment.model';
+import { TaskdepartmentService } from '../services/taskdepartment.service';
 
 @Component({
   selector: 'app-taskdepartment-detail',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskdepartmentDetailComponent implements OnInit {
 
-  constructor() { }
+  taskdepartment: Taskdepartment | undefined;
+
+  constructor(
+    private taskdepartmentService: TaskdepartmentService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe({
+      next: pmap => {
+        let id = pmap.get("id");
+        this.fetchTaskdepartment(id);
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  fetchTaskdepartment(id: string | null) {
+    this.taskdepartmentService.findByIdTaskDepartment(Number(id)).subscribe({
+      next: taskdepartmentBackend => this.taskdepartment = taskdepartmentBackend,
+      error: err => console.log(err)
+    });
+  }
+
+  onDelete(id: number) {
+    console.log(id);
+    this.taskdepartmentService.deleteByIdTaskdepartment(id).subscribe({
+      next: response => this.navigateToList(),
+      error: err => console.log(err)
+    });
+  }
+  //Despues de eliminar vuelve a la lista
+  private navigateToList() {
+    this.router.navigate(["/taskdepartments"]);
   }
 
 }
